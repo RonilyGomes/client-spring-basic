1ad33d070638ed61e80dfabd9e384193532d86c2
Ronily <ronily.web@gmail.com>
2019-02-18 13:25:30 -0300

    ajustes finais no projeto

--- js/ajax.js
+++ js/ajax.js
@@ -1,29 +1,53 @@
-$(document).ready(function(){
-  loadPeople();
+const CREATED = 'CREATED';
+const CONFLICT = 'CONFLICT';
+const NOT_FOUND = 'NOT_FOUND';
+const OK = 'OK';
 
-  $("#person_save").click(function() {
-    savePerson();
+$(document).ready(function(){
+  loadNews();
+
+  $("#news_save").click(function() {
+    saveNews();
   });
 });
 
-function loadPeople() {
+function loadNews() {
   $.ajax({
-    url : "http://127.0.0.1:3000/api/v1/people",
+    url : "http://127.0.0.1:8080/api/v1/news",
     type : 'get',
     dataType: 'json',
   })
   .done(function(response){
-    $("#dataTable").html(generateRowsOfTable(response));
+    console.log(response);
+    $("#dataTable").html(generateRowsOfTable(response.data));
   })
-  .fail(function(jqXHR, textStatus, msg){
+  .fail(function(){
     alert("Ocorreu um erro inesperado!");
   });
 }
 
-function generateRowsOfTable(people) {
+function loadSingleNews(id) {
+  $.ajax({
+    // URL abaixo para testar noticia não encontrada
+    //url : `http://127.0.0.1:8080/api/v1/news?id=500`,
+    url : `http://127.0.0.1:8080/api/v1/news?id=${id}`,
+    type : 'get',
+    dataType: 'html',
+  })
+  .done(function(response){
+    console.log(response);
+    $("#editEmployeeModal").html(response);
+  })
+  .fail(function(xhr, ajaxOptions, thrownError){
+    alert(extractErrorFromResponseHtml(xhr.responseText));
+    $("#editEmployeeModal").modal("hide")
+  });
+}
+
+function generateRowsOfTable(news) {
   result = '';
 
-  $.each(people, function (i, person) {
+  $.each(news, function (i, n) {
     result += `<tr>
       <td>
         <span class="custom-checkbox">
@@ -31,15 +55,15 @@ function generateRowsOfTable(people) {
           <label for="checkbox1"></label>
         </span>
       </td>
-      <td>${person.name}</td>
-      <td>${person.nickname}</td>
-      <td>${person.email}</td>
-      <td>${person.phone}</td>
+      <td>${n.author}</td>
+      <td>${n.title}</td>
+      <td>${n.postDate}</td>
+      <td>${n.content}</td>
       <td>
-        <a href="#editEmployeeModal" class="edit" data-toggle="modal" onclick="loadPerson(${person.id})">
+        <a href="#editEmployeeModal" class="edit" data-toggle="modal" onclick="loadSingleNews(${n.id})">
           <i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
         </a>
-        <a href="#deleteEmployeeModal" class="delete" data-toggle="modal" onclick="loadModalFooterPersonDelete(${person.id})">
+        <a href="#deleteEmployeeModal" class="delete" data-toggle="modal" onclick="loadModalFooterSingleNewsDelete(${n.id})">
           <i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
         </a>
       </td>
@@ -49,121 +73,98 @@ function generateRowsOfTable(people) {
   return result;
 }
 
-function savePerson() {
-  let name = $("#person_name").val();
-  let nickname = $("#person_nickname").val();
-  let email = $("#person_email").val();
-  let phone = $("#person_phone").val();
+function saveNews() {
+  let author    = $("#news_author").val();
+  let title     = $("#news_title").val();
+  let content   = $("#news_content").val();
 
   $.ajax({
-    url : "http://127.0.0.1:3000/api/v1/people",
+    url : "http://127.0.0.1:8080/api/v1/news",
     type : 'POST',
     dataType: 'json',
     contentType: "application/x-www-form-urlencoded",
     data: {
-      name: name,
-      nickname: nickname,
-      email: email,
-      phone: phone
+      author: author,
+      title: title,
+      content: content
     },
   })
   .done(function(response){
-    $("#addEmployeeModal").modal('hide');
+    console.log(response);
     alert("cadastrado realizado com sucesso!");
-    loadPeople();
+    $("#addEmployeeModal").modal('hide');
+    loadNews();
+    clearFieldModalSave();
   })
-  .fail(function(error){
-    alert("Ocorreu um erro inesperado");
+  .fail(function(xhr, ajaxOptions, thrownError){
+    alert(extractErrorFromResponse(xhr.responseText));
   });
 }
 
-function loadPerson(id) {
-  $.ajax({
-    url : `http://127.0.0.1:3000/api/v1/people/${id}`,
-    type : 'get',
-    dataType: 'json',
-  })
-  .done(function(response){
-    $("#modal-body-edit").html(loadDataFromPerson(response));
-    $("#modal-footer-edit").html(loadModalFooterPersonEdit(response.id));
-  })
-  .fail(function(jqXHR, textStatus, msg){
-    alert("Ocorreu um erro inesperado");
-  });
+function clearFieldModalSave() {
+  $("#news_author").val("");
+  $("#news_title").val("");
+  $("#news_content").val("");  
 }
 
-function loadDataFromPerson(person) {
-    return `<div class="form-group">
-      <label>Nome</label>
-      <input type="text" class="form-control" name="name" id="person_name_edit" required value="${person.name}">
-    </div>
-    <div class="form-group">
-      <label>Apelido</label>
-      <input type="text" class="form-control" name="nickname" id="person_nickname_edit" required value="${person.nickname}">
-    </div>
-    <div class="form-group">
-      <label>E-mail</label>
-      <input type="email" class="form-control" name="email" id="person_email_edit" required value="${person.email}">
-    </div>
-    <div class="form-group">
-      <label>Phone</label>
-      <input type="text" class="form-control" name="phone" id="person_phone_edit" required value="${person.phone}">
-    </div>`;
-}
-
-function loadModalFooterPersonEdit(id) {
-  return `<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
-  <input type="submit" class="btn btn-success"
-  onclick="event.preventDefault(); editPerson(${id})" value="Alterar" id="person_edit">`
-}
-
-function editPerson(id) {
-  let name = $("#person_name_edit").val();
-  let nickname = $("#person_nickname_edit").val();
-  let email = $("#person_email_edit").val();
-  let phone = $("#person_phone_edit").val();
+function editSingleNews(id) {
+  let title     = $("#news_title_edit").val();
 
   $.ajax({
-    url : `http://127.0.0.1:3000/api/v1/people/${id}`,
+    // URL abaixo para testar noticia não encontrada
+    //url : `http://127.0.0.1:8080/api/v1/news/500/title/${title}`,
+    url : `http://127.0.0.1:8080/api/v1/news/${id}/title/${title}`,
     type : 'PUT',
     dataType: 'json',
-    contentType: "application/x-www-form-urlencoded",
-    data: {
-      name: name,
-      nickname: nickname,
-      email: email,
-      phone: phone
-    },
+    contentType: "application/x-www-form-urlencoded"
   })
   .done(function(response){
+    console.log(response);
     $("#editEmployeeModal").modal('hide');
     alert("Edição realizada com sucesso!");
-    loadPeople();
+    loadNews();
   })
-  .fail(function(error){
-    alert("Ocorreu um erro inesperado");
+  .fail(function(xhr, ajaxOptions, thrownError){
+    alert(extractErrorFromResponse(xhr.responseText));
   });
 }
 
-function loadModalFooterPersonDelete(id) {
-  $("#modal-footer-delete").html(`<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
-  <input type="submit" class="btn btn-danger"
-  onclick="event.preventDefault(); deletePerson(${id})" value="Remover" id="person_delete">`);
-}
-
-function deletePerson(id) {
+function deleteSingleNews(id) {
   $.ajax({
-    url : `http://127.0.0.1:3000/api/v1/people/${id}`,
+    // URL abaixo para testar noticia não encontrada
+    //url : `http://127.0.0.1:8080/api/v1/news/500`,
+    url : `http://127.0.0.1:8080/api/v1/news/${id}`,
     type : 'DELETE',
-    dataType: 'json',
-    contentType: "application/x-www-form-urlencoded",
+    dataType: 'xml',
+    contentType: "application/xml",
   })
   .done(function(response){
+    console.log(response);
     $("#deleteEmployeeModal").modal('hide');
-    alert("Pessoa removida com sucesso!");
-    loadPeople();
+    alert("Notícia removida com sucesso!");
+    loadNews();
   })
-  .fail(function(error){
-    alert("Ocorreu um erro inesperado");
+  .fail(function(xhr, ajaxOptions, thrownError){
+    alert(extractErrorFromResponseXml(xhr.responseText));
   });
+}
+
+function loadModalFooterSingleNewsDelete(id) {
+  $("#modal-footer-delete").html(`
+    <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
+    <input type="submit" class="btn btn-danger"
+    onclick="event.preventDefault(); deleteSingleNews(${id})" value="Remover" id="news_delete">`
+  );
+}
+
+function extractErrorFromResponse(response) {
+  return eval("(" + response + ")").message;
+}
+
+function extractErrorFromResponseXml(response) {
+  return $(response).find('message').text();
+}
+
+function extractErrorFromResponseHtml(response) {
+  return $(response).text(); 
 }
